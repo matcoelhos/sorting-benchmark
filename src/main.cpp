7db68c94d497cfa39dd3996ed925815f98f62d52
Mateus Coelho <matcoelhosilva@yahoo.com.br>
2019-05-21 15:59:46 -0300

    Change in main to support CPU affinity selection.

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,37 @@
 #include "QuickSort.h"
 #include "MergeSort.h"
 #include "InsertionSort.h"
+#include <assert.h>
+#include <sched.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 bool getVerboseOption(int argc, char *argv[]);
 
 std::string toLower(string algorithm);
 
+void print_affinity() {
+    cpu_set_t mask;
+    long nproc, i;
+
+    if (sched_getaffinity(0, sizeof(cpu_set_t), &mask) == -1) {
+        perror("sched_getaffinity");
+        assert(false);
+    } else {
+        nproc = sysconf(_SC_NPROCESSORS_ONLN);
+        printf("sched_getaffinity = ");
+        for (i = 0; i < nproc; i++) {
+            printf("%d ", CPU_ISSET(i, &mask));
+        }
+        printf("\n");
+    }
+}
 
 int main(int argc, char *argv[]) {
 
+    cpu_set_t mask;
     if (argc >= 5) {
 
         bool isVerbose = getVerboseOption(argc, argv);
@@ -20,6 +43,9 @@ int main(int argc, char *argv[]) {
         char *inputFile = argv[2];
         char *outputFile = argv[3];
         std::string algorithm = toLower(argv[4]);
+
+        CPU_ZERO(&mask);
+	      CPU_SET(atoi(argv[5]), &mask);
 
         if (algorithm == QUICK_SORT) {
             QuickSort qs(arraySize, inputFile, outputFile, isVerbose);
